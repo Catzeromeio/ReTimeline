c98a97058cb1df08bd101e3e1832f194f3eae87a
catzero <catzeromeio@hotmail.com>
2022-01-07 22:56:00 +0800

    feat: 基础窗口

---
+++ Editor/Window/ReTimelineWindow.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEditor.Callbacks;
+using UnityEngine.Events;
+using UnityEngine.Playables;
+using UnityEngine.SceneManagement;
+
+
+[EditorWindowTitle(title = "ReTimeline", useTypeNameAsIconName = true)]
+public class ReTimelineWindow : EditorWindow
+{
+    public static ReTimelineWindow instance { get; private set; }
+
+    private void OnEnable()
+    {
+        if (instance == null)
+            instance = this;
+    }
+
+    [MenuItem("Window/Sequencing/ReTimeline", false, 2)]
+    public static void ShowWindow()
+    {
+        GetWindow<ReTimelineWindow>(typeof(SceneView));
+        instance.Focus();
+    }
+}
